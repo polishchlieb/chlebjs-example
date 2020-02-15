468b22d08439008fe2358dfeaf6567e42dc44adb
polishchlieb <biczu.yt@gmail.com>
2020-02-16 00:39:54 +0100

    experimental

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -3,15 +3,38 @@ import store from '../store';
 
 const [state, setState, subscribe] = store;
 
+const $style = document.createElement('style');
+document.querySelector('head').appendChild($style);
+
+const possible = 'abcdefghijklmnopqrstuwxyz';
+function random() {
+    let str = '';
+    for (let i = 0; i < 10; i++)
+        str += possible[Math.floor(Math.random() * possible.length)];
+    return str;
+}
+
 export default class Counter extends Component {
+    ref = Pixel.createRef();
+
     constructor() {
         super();
-        subscribe(() => this.renderComponent());
+        subscribe(() => {
+            this.renderComponent();
+            
+            const name = random();
+            $style.sheet.insertRule(`
+                .${name} {
+                    color: ${'#' + Math.floor(Math.random()*16777215).toString(16)}
+                }
+            `);
+            this.ref.current.className = name;
+        });
     }
 
     render() {
         return (
-            <h1>{String(state.counter)}</h1>
+            <h1 ref={this.ref}>{String(state.counter)}</h1>
         );
     }
 }
